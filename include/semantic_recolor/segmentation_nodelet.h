f05e87c06d3bf6c1fbbad77d535d43c6ce579fdf
Nathan Hughes <nathan.h.hughes@gmail.com>
2022-11-17 22:41:40 +0000

    add time-based throttle directly to nodelet

--- include/semantic_recolor/segmentation_nodelet.h
+++ include/semantic_recolor/segmentation_nodelet.h
@@ -1,11 +1,15 @@
 #pragma once
+#include "semantic_recolor/model_config.h"
 #include "semantic_recolor/nodelet_output_publisher.h"
 #include "semantic_recolor/segmenter.h"
-#include "semantic_recolor/model_config.h"
 
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 #include <nodelet/nodelet.h>
+
+#include <thread>
+#include <mutex>
+#include <atomic>
 
 namespace semantic_recolor {
 
@@ -13,13 +17,28 @@ class SegmentationNodelet : public nodelet::Nodelet {
  public:
   virtual void onInit() override;
 
+  virtual ~SegmentationNodelet();
+
  private:
+  void spin();
+
+  void runSegmentation(const sensor_msgs::ImageConstPtr& msg);
+
   void callback(const sensor_msgs::ImageConstPtr& msg);
+
+  bool haveWork() const;
 
   ModelConfig config_;
   std::unique_ptr<TrtSegmenter> segmenter_;
   std::unique_ptr<image_transport::ImageTransport> transport_;
   image_transport::Subscriber image_sub_;
+
+  int max_queue_size_;
+  double image_separation_s_;
+  std::list<sensor_msgs::ImageConstPtr> image_queue_;
+  mutable std::mutex queue_mutex_;
+  std::atomic<bool> should_shutdown_;
+  std::unique_ptr<std::thread> spin_thread_;
 
   std::unique_ptr<NodeletOutputPublisher> output_pub_;
 };
--- src/segmentation_nodelet.cpp
+++ src/segmentation_nodelet.cpp
@@ -2,12 +2,20 @@
 #include "semantic_recolor/ros_utilities.h"
 
 #include <pluginlib/class_list_macros.h>
+#include <optional>
 
 PLUGINLIB_EXPORT_CLASS(semantic_recolor::SegmentationNodelet, nodelet::Nodelet)
 
 namespace semantic_recolor {
+
 void SegmentationNodelet::onInit() {
   ros::NodeHandle& pnh = getPrivateNodeHandle();
+
+  max_queue_size_ = 30;
+  pnh.getParam("max_queue_size", max_queue_size_);
+
+  image_separation_s_ = 0.0;
+  pnh.getParam("image_separation_s", image_separation_s_);
 
   config_ = readModelConfig(pnh);
   segmenter_.reset(new TrtSegmenter(config_));
@@ -22,9 +30,20 @@ void SegmentationNodelet::onInit() {
 
   image_sub_ =
       transport_->subscribe("rgb/image_raw", 1, &SegmentationNodelet::callback, this);
+
+  should_shutdown_ = false;
+  spin_thread_.reset(new std::thread(&SegmentationNodelet::spin, this));
 }
 
-void SegmentationNodelet::callback(const sensor_msgs::ImageConstPtr& msg) {
+SegmentationNodelet::~SegmentationNodelet() {
+  should_shutdown_ = true;
+  if (spin_thread_) {
+    spin_thread_->join();
+    spin_thread_.reset();
+  }
+}
+
+void SegmentationNodelet::runSegmentation(const sensor_msgs::ImageConstPtr& msg) {
   cv_bridge::CvImageConstPtr img_ptr;
   try {
     img_ptr = cv_bridge::toCvShare(msg);
@@ -43,6 +62,56 @@ void SegmentationNodelet::callback(const sensor_msgs::ImageConstPtr& msg) {
   }
 
   output_pub_->publish(img_ptr->header, img_ptr->image, segmenter_->getClasses());
+}
+
+bool SegmentationNodelet::haveWork() const {
+  std::unique_lock<std::mutex> lock(queue_mutex_);
+  return !image_queue_.empty();
+}
+
+void SegmentationNodelet::spin() {
+  const ros::Duration min_diff(image_separation_s_);
+
+  ros::WallRate r(100);
+  std::optional<ros::Time> last_time;
+  while (ros::ok() && !should_shutdown_) {
+    ros::spinOnce();
+    if (!haveWork()) {
+      r.sleep();
+      continue;
+    }
+
+    sensor_msgs::ImageConstPtr msg;
+    {  // start mutex scope
+      std::unique_lock<std::mutex> lock(queue_mutex_);
+      if (last_time) {
+        const auto curr_diff = image_queue_.front()->header.stamp - *last_time;
+        ROS_INFO_STREAM("current time diff: " << curr_diff.toSec() << "[s] (min: "
+                                              << min_diff.toSec() << "[s])");
+        if (curr_diff < min_diff) {
+          image_queue_.pop_front();
+          continue;
+        }
+      }
+
+      last_time = image_queue_.front()->header.stamp;
+      msg = image_queue_.front();
+      image_queue_.pop_front();
+
+    }  // end mutex scope
+
+    runSegmentation(msg);
+  }
+}
+
+void SegmentationNodelet::callback(const sensor_msgs::ImageConstPtr& msg) {
+  std::unique_lock<std::mutex> lock(queue_mutex_);
+  if (max_queue_size_ > 0 &&
+      image_queue_.size() >= static_cast<size_t>(max_queue_size_)) {
+    image_queue_.pop_front();
+  }
+
+  image_queue_.push_back(msg);
 }
 
 }  // namespace semantic_recolor
