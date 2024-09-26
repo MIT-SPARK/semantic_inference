aa10d6b424bde5888ab530ab740f644b48832d57
Nathan Hughes <na26933@mit.edu>
2024-09-26 14:44:16 -0400

    Feature/better color legend (#29)
    * move grid-based legend over
    
    * clean up color palette and legend generation
    
    * fix plotting
    
    Co-authored-by: Nathan Hughes <nathan.h.hughes@gmail.com>

--- semantic_inference/python/semantic_inference/commands/color.py
+++ semantic_inference/python/semantic_inference/commands/color.py
@@ -1,236 +1,21 @@
 """Subcommands for handling colormaps."""
 
 import csv
-import math
 import pathlib
 import pprint
-import random
 import sys
 
 import click
-import distinctipy
-import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import yaml
-from matplotlib.patches import Rectangle
-
-EXPANDED_COLORS = {
-    "black": [0, 0, 0],
-    "white": [255, 255, 255],
-    "blue": [0, 0, 225],
-    "orange": [255, 140, 0],
-    "green": [0, 200, 0],
-    "red": [225, 0, 0],
-    "purple": [120, 0, 120],
-    "brown": [139, 69, 19],
-    "grey": [128, 128, 128],
-    "yellow": [225, 225, 0],
-    "magenta": [225, 0, 225],
-    "cyan": [0, 255, 255],
-    "lightgrey": [220, 220, 220],
-    "darkgrey": [100, 100, 100],
-    "lightblue": [135, 206, 235],
-    "darkblue": [0, 0, 128],
-    "lightgreen": [0, 255, 0],
-    "darkgreen": [0, 108, 0],
-    "lightred": [255, 120, 120],
-    "darkred": [128, 0, 0],
-    "lightpurple": [186, 85, 211],
-    "darkpurple": [75, 0, 130],
-}
+from semantic_inference.visualization import (make_colormap_legend,
+                                              make_palette, verify_palette)
 
 
-def _read_label_config(category_groups_path):
-    with category_groups_path.open("r") as fin:
-        config = yaml.load(fin.read(), Loader=yaml.SafeLoader)
-    return config
-
-
-def _int_color(color):
-    return [int(math.floor(255.0 * x)) for x in color[:3]]
-
-
-def _colors_equal(c1, c2):
-    c1_int = _int_color(c1)
-    c2_int = _int_color(c2)
-    return c1_int == c2_int
-
-
-def _verify_palette(colors_by_group):
-    invalid_groups = set([])
-    for group, color in colors_by_group.items():
-        for other_group, other_color in colors_by_group.items():
-            if group == other_group:
-                continue
-
-            if not _colors_equal(color, other_color):
-                continue
-
-            invalid_groups.add(group)
-            invalid_groups.add(other_group)
-            click.secho(
-                f"[ERROR]: {group} and {other_group} share color {_int_color(color)}",
-                fg="red",
-            )
-
-    return len(invalid_groups) == 0
-
-
-def _get_specified_color(group_color):
-    if group_color["source"] == "raw":
-        return [float(x) for x in group_color["color"][:3]]
-
-    if group_color["source"] == "raw_int":
-        return [float(x) / 255.0 for x in group_color["color"][:3]]
-
-    color_name = group_color["color"]
-    if group_color["source"] == "expanded":
-        if color_name not in EXPANDED_COLORS:
-            click.secho(f"failed to find {color_name} in EXPANDED_COLORS", fg="red")
-            return None
-
-        return [x / 255.0 for x in EXPANDED_COLORS[color_name][:3]]
-
-    if group_color["source"] == "mpl":
-        if color_name not in mcolors.CSS4_COLORS:
-            click.secho(f"failed to find {color_name} in CSS4_COLORS", fg="red")
-            return None
-
-        return [x for x in mcolors.to_rgb(mcolors.CSS4_COLORS[color_name])]
-
-
-def _get_palette(label_config, palette, seed, force_compatible=False):
-    colors_by_group = {
-        group_color["name"]: _get_specified_color(group_color)
-        for group_color in label_config["specified_colors"]
-    }
-    N_groups = len(label_config["groups"])
-    num_to_assign = N_groups if force_compatible else N_groups - len(colors_by_group)
-
-    base_colors = []
-    if "palette" in label_config:
-        for color in label_config["palette"]:
-            base_colors.append([float(x) for x in color])
-
-    if len(base_colors) < num_to_assign:
-        if "palette" in label_config:
-            click.secho(
-                "[WARN]: invalid palette size ({len(base_colors)} < {num_to_assign}",
-                fg="yellow",
-            )
-
-        distinct = label_config.get("distinct", False)
-        if not distinct:
-            base_colors = sns.color_palette(palette, num_to_assign)
-        else:
-            group_colors = [v for _, v in colors_by_group.items()]
-            chosen_colors = base_colors + group_colors
-            num_to_find = N_groups - len(chosen_colors)
-            # we also don't want white and black as colors
-            new_colors = distinctipy.get_colors(
-                num_to_find,
-                exclude_colors=chosen_colors + [(0.0, 0.0, 0.0), (1.0, 1.0, 1.0)],
-            )
-            base_colors += new_colors
-
-    random.seed(seed)
-    random.shuffle(base_colors)
-
-    color_index = 0
-    for group in label_config["groups"]:
-        group_name = group["name"]
-
-        if group_name in colors_by_group:
-            if force_compatible:
-                color_index += 1
-            continue
-
-        colors_by_group[group_name] = [float(x) for x in base_colors[color_index][:3]]
-        color_index += 1
-
-    return colors_by_group
-
-
-def _write_config(label_config, group_colors, output_dir, config_name, csv_shift=0):
-    csv_output = output_dir / f"{config_name}.csv"
-    with csv_output.open("w") as fout:
-        fout.write("name,red,green,blue,alpha,id\r\n")
-        for index, group in enumerate(label_config["groups"]):
-            name = group["name"]
-            color = _int_color(group_colors[name])
-            idx = index + csv_shift
-            fout.write(f"{name},{color[0]},{color[1]},{color[2]},255,{idx}\r\n")
-
-
-# TODO(nathan) use better implementation
-# adapted from https://matplotlib.org/stable/gallery/color/named_colors.html
-def _plot_colortable(
-    color_dict,
-    name_dict,
-    sort_colors=True,
-    desiredcols=4,
-    emptycols=0,
-    cell_width=205,
-    cell_height=22,
-    swatch_width=22,
-    margin=12,
-    dpi=300,
-    alpha=0.8,
-):
-    N = len(color_dict)
-    ncols = desiredcols - emptycols
-    nrows = N // ncols + int(N % ncols > 0)
-
-    width = cell_width * ncols + 2 * margin
-    height = cell_height * nrows + 2 * margin
-
-    fig, ax = plt.subplots(figsize=(width / dpi, height / dpi), dpi=dpi)
-    fig.subplots_adjust(
-        margin / width,
-        margin / height,
-        (width - margin) / width,
-        (height - margin) / height,
-    )
-    ax.set_xlim(0, cell_width * ncols)
-    ax.set_ylim(cell_height * (nrows - 0.5), -cell_height / 2.0)
-    ax.yaxis.set_visible(False)
-    ax.xaxis.set_visible(False)
-    ax.set_axis_off()
-
-    labels = sorted([x for x in color_dict])
-    for i, label in enumerate(labels):
-        row = i % nrows
-        col = i // nrows
-        y = row * cell_height
-
-        swatch_start_x = cell_width * col
-        text_pos_x = cell_width * col + swatch_width + 7
-
-        ax.text(
-            text_pos_x,
-            y,
-            f"{name_dict[label]}",
-            fontsize=14,
-            horizontalalignment="left",
-            verticalalignment="center",
-        )
-
-        color = color_dict[label].tolist() + [alpha]
-        ax.add_patch(
-            Rectangle(
-                xy=(swatch_start_x, y - 9),
-                width=swatch_width,
-                height=18,
-                facecolor=color,
-                edgecolor="k",
-            )
-        )
-
-    plt.tight_layout()
-    return fig
+def _normalize_path(path):
+    return pathlib.Path(path).expanduser().absolute()
 
 
 @click.group(name="color")
@@ -241,53 +26,37 @@ def cli():
 
 @cli.command()
 @click.argument("categories", type=click.Path(exists=True))
-@click.argument("output", type=click.Path(exists=True))
-@click.option("-p", "--palette", default="husl", help="fallback color palette")
-@click.option("-s", "--seed", default=0, type=int, help="random seed")
+@click.argument("output", type=click.Path())
+@click.option("-s", "--seed", default=None, type=int, help="random seed")
 @click.option("-n", "--config-name", default=None, help="output config name")
-@click.option("--csv-shift", type=int, default=0, help="amount to add to color csv")
-@click.option(
-    "-f",
-    "--force-compatible",
-    default=False,
-    is_flag=True,
-    help="use same palette size as original scripts",
-)
-def create(
-    categories,
-    output,
-    palette,
-    seed,
-    config_name,
-    csv_shift,
-    force_compatible,
-):
+def create(categories, output, seed):
     """Create a color map from CATEGORIES and output it to OUTPUT."""
-    category_groups_path = pathlib.Path(categories).expanduser().absolute()
-    output_path = pathlib.Path(output).expanduser().absolute()
+    categories = _normalize_path(categories)
+    output = _normalize_path(output)
 
-    label_config = _read_label_config(category_groups_path)
-    colors = _get_palette(
-        label_config, palette, seed, force_compatible=force_compatible
-    )
-    if not _verify_palette(colors):
+    with categories.open("r") as fin:
+        config = yaml.load(fin.read(), Loader=yaml.SafeLoader)
+
+    base_colors = []
+    if "palette" in config:
+        for color in config["palette"]:
+            base_colors.append([int(x) for x in color])
+
+    # TODO(nathan) convert label config to a better input to make_palette
+    colors = make_palette(config, base_palette=base_colors, seed=seed)
+    if not verify_palette(colors):
         click.secho("[FATAL]: color collision in color palette", fg="red")
         sys.exit(1)
 
-    name_to_use = (
-        config_name if config_name is not None else label_config.get("name", "test")
-    )
-    if name_to_use == "test":
-        click.secho(
-            "[WARN]: config name not specifying, defaulting to test", fg="yellow"
-        )
-    _write_config(
-        label_config,
-        colors,
-        output_path,
-        name_to_use,
-        csv_shift=csv_shift,
-    )
+    if not output.parent.exists():
+        output.parent.mkdir(parents=True)
+
+    with output.open("w") as fout:
+        fout.write("name,red,green,blue,alpha,id\r\n")
+        for idx, group in enumerate(config["groups"]):
+            name = group["name"]
+            color = colors["name"]
+            fout.write(f"{name},{color[0]},{color[1]},{color[2]},255,{idx}\r\n")
 
 
 @cli.command()
@@ -307,33 +76,75 @@ def show(colormap):
 
 @cli.command()
 @click.argument("colormap", type=click.Path(exists=True))
-@click.option("-c", "--num-cols", type=int, default=4)
-@click.option("-d", "--dpi", type=float, default=100.0)
-@click.option("-o", "--output", default=None)
-@click.option("-a", "--alpha", type=float, default=0.8)
-def plot(colormap, num_cols, dpi, output, alpha):
-    """Plot COLORMAP showing correspondences between names and colors."""
-    color_dict = {}
-    name_dict = {}
+@click.option("-l", "--labelspace", type=click.Path(exists=True), help="label to names")
+@click.option("-o", "--output", default=None, help="output filepath")
+@click.option("-p", "--plot", is_flag=True, help="plot legend")
+@click.option("--skip-header", is_flag=True, help="skip first csv row")
+@click.option("-r", "--num-rows", type=int, default=None, help="number of grid rows")
+@click.option("-c", "--num-cols", type=int, default=5, help="number of grid columns")
+@click.option("-d", "--dpi", type=float, default=600.0, help="output DPI")
+@click.option("-a", "--alpha", type=float, default=1.0, help="visualizer alpha")
+@click.option("-w", "--cell-width", type=float, default=600, help="cell width (px)")
+@click.option("-h", "--cell-height", type=float, default=200, help="cell height (px)")
+@click.option("-f", "--fontsize", type=int, default=12, help="text fontsize")
+@click.option("-t", "--wrap", type=int, default=14, help="characters before wrapping")
+@click.option("--offset", type=float, default=10, help="pixel offset for text")
+def legend(
+    colormap,
+    labelspace,
+    output,
+    plot,
+    skip_header,
+    num_rows,
+    num_cols,
+    dpi,
+    alpha,
+    cell_width,
+    cell_height,
+    fontsize,
+    wrap,
+    offset,
+):
+    """Create a legend for COLORMAP showing correspondences between names and colors."""
+    label_to_name = {}
+    label_to_color = {}
 
-    sns.set()
-    sns.set_style("white")
-    sns.set_context("paper", font_scale=1.0)
+    def _read_color(line):
+        return np.squeeze(np.array([float(x) for x in line[1:4]]) / 255.0)
 
-    csv_path = pathlib.Path(colormap)
-    with csv_path.open("r") as fin:
+    with _normalize_path(colormap).open("r") as fin:
         reader = csv.reader(fin)
         next(reader)
-
         for line in reader:
+            name = line[0]
             label = int(line[-1])
-            name_dict[label] = line[0]
-            color_dict[label] = np.squeeze(
-                np.array([float(x) for x in line[1:4]]) / 255
-            )
+            label_to_name[label] = name
+            label_to_color[label] = _read_color(line)
 
-    _plot_colortable(color_dict, name_dict, desiredcols=num_cols, dpi=dpi, alpha=alpha)
-    if output is None:
-        plt.show()
-    else:
-        plt.savefig(output, bbox_inches="tight", dpi=300)
+    if labelspace is not None:
+        label_to_name = {}
+        with _normalize_path(labelspace).open("r") as fin:
+            contents = yaml.safe_load(fin.read())
+            for x in contents["label_names"]:
+                label_to_name[x["label"]] = x["name"]
+
+    name_to_color = {v: label_to_color[k] for k, v in label_to_name.items()}
+    fig = make_colormap_legend(
+        name_to_color,
+        nrows=num_rows,
+        ncols=num_cols,
+        dpi=dpi,
+        alpha=alpha,
+        cell_width=cell_width,
+        cell_height=cell_height,
+        fontsize=fontsize,
+        offset=offset,
+        wrapsize=wrap,
+    )
+
+    if plot:
+        fig.show()
+        plt.show(block=True)
+
+    if output is not None:
+        fig.savefig(_normalize_path(output), dpi=dpi)
---
+++ semantic_inference/python/semantic_inference/visualization.py
@@ -0,0 +1,266 @@
+"""Various visualization utilities."""
+
+import functools
+import logging
+import math
+import random
+
+import distinctipy
+import matplotlib.colors
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+# useful colornames
+EXPANDED_COLORS = {
+    "black": [0, 0, 0],
+    "white": [255, 255, 255],
+    "blue": [0, 0, 225],
+    "orange": [255, 140, 0],
+    "green": [0, 200, 0],
+    "red": [225, 0, 0],
+    "purple": [120, 0, 120],
+    "brown": [139, 69, 19],
+    "grey": [128, 128, 128],
+    "yellow": [225, 225, 0],
+    "magenta": [225, 0, 225],
+    "cyan": [0, 255, 255],
+    "lightgrey": [220, 220, 220],
+    "darkgrey": [100, 100, 100],
+    "lightblue": [135, 206, 235],
+    "darkblue": [0, 0, 128],
+    "lightgreen": [0, 255, 0],
+    "darkgreen": [0, 108, 0],
+    "lightred": [255, 120, 120],
+    "darkred": [128, 0, 0],
+    "lightpurple": [186, 85, 211],
+    "darkpurple": [75, 0, 130],
+}
+
+
+def _int_color(color):
+    return [int(math.floor(255.0 * x)) for x in color[:3]]
+
+
+def _float_color(color):
+    return [float(x) / 255.0 for x in color[:3]]
+
+
+def _colors_equal(c1, c2):
+    c1_int = _int_color(c1)
+    c2_int = _int_color(c2)
+    return c1_int == c2_int
+
+
+def _line_length(x):
+    return len(x) + sum([len(w) for w in x])
+
+
+def _wrap(label, width=15):
+    words = label.split(" ")
+    lines = []
+
+    line = []
+    for word in words:
+        if len(line) == 0:
+            line.append(word)
+            continue
+
+        curr_length = len(word) + _line_length(line)
+        if curr_length > width:
+            lines.append(" ".join(line))
+            line = [word]
+        else:
+            line.append(word)
+
+    if len(line) > 0:
+        lines.append(" ".join(line))
+
+    return "\n".join(lines)
+
+
+def make_color(color_type, color_info):
+    """
+    Make an RGB color from provided information.
+
+    Accepts various color types:
+      - `raw`: `color_info` is a 3-channel RGB value between 0 and 1
+      - 'raw_int`: `color_info` is a 3-channel integer RGB value between 0 and 255
+      - `expanded`: `color_info` is a color name from the EXPANDED_COLORS map
+      - `mpl`: `color_info` is a color name in the CSS4_COLORS palette
+
+    Args:
+        color_type (str): Color "factory" to use
+        color_info (Union[List[Union[int, float]], str]): Raw RGB values or color name
+
+    Returns:
+        List[float]: Three-channel RGB value between 0 and 1.
+    """
+    if color_type == "raw":
+        return [float(x) for x in color_info[:3]]
+
+    if color_type == "raw_int":
+        return _float_color(color_info)
+
+    if color_type == "expanded":
+        color = EXPANDED_COLORS.get(color_info)
+        if color is None:
+            logging.warning(f"failed to find '{color_info}' in EXPANDED_COLORS")
+            return None
+
+        return [x / 255.0 for x in color]
+
+    if color_type == "mpl":
+        color = matplotlib.colors.CSS4_COLORS.get(color_info)
+        if color is not None:
+            logging.warning(f"failed to find {color_info} in CSS4_COLORS")
+            return None
+
+        return [x for x in matplotlib.colors.to_rgb(color)]
+
+    logging.warning(f"invalid color type '{color_type}'")
+    return None
+
+
+def make_palette(label_config, base_colors=None, seed=None):
+    """Make a color palette for the provided label space."""
+    colors_by_group = {
+        group["name"]: make_color(group.get("source"), group.get("color"))
+        for group in label_config["specified_colors"]
+    }
+    colors_by_group = {k: v for k, v in colors_by_group.items() if v is not None}
+    N_groups = len(label_config["groups"])
+    N_new = N_groups - len(colors_by_group)
+
+    palette = [_float_color(c) for c in base_colors] if base_colors is not None else []
+    if len(base_colors) < N_new:
+        if base_colors is not None:
+            logging.warning("invalid palette size ({len(base_colors)} < {N_new}")
+
+        group_colors = [v for _, v in colors_by_group.items()]
+        chosen_colors = base_colors + group_colors
+        num_to_find = N_groups - len(chosen_colors)
+        # we also don't want white and black as colors
+        new_colors = distinctipy.get_colors(
+            num_to_find,
+            exclude_colors=chosen_colors + [(0.0, 0.0, 0.0), (1.0, 1.0, 1.0)],
+        )
+        base_colors += new_colors
+
+    if seed is not None:
+        random.seed(seed)
+        random.shuffle(palette)
+
+    colors = [_int_color(x) for x in palette]
+
+    color_index = 0
+    for group in label_config["groups"]:
+        group_name = group["name"]
+
+        if group_name in colors_by_group:
+            continue
+
+        colors_by_group[group_name] = [float(x) for x in colors[color_index][:3]]
+        color_index += 1
+
+    return colors_by_group
+
+
+def verify_palette(color_palette):
+    """Check that every label has a distinct color."""
+    invalid = set([])
+    for name, color in color_palette.items():
+        for other, other_color in color_palette.items():
+            if name == other:
+                continue
+
+            if not _colors_equal(color, other_color):
+                continue
+
+            invalid.add(name)
+            invalid.add(other)
+            logging.error(f"[ERROR]: {name} and {other} share color {color}")
+
+    return len(invalid) == 0
+
+
+def make_colormap_legend(
+    colormap,
+    nrows=None,
+    ncols=4,
+    cell_width=60,
+    cell_height=22,
+    dpi=300,
+    fontsize=8,
+    wrapsize=14,
+    offset=1,
+    alpha=1.0,
+    sort=False,
+):
+    """
+    Create a color legend using.
+
+    Args:
+        colormap (Dict[str, Tuple[float, float, float]]): Map between names and colors
+        nrows (int): Number of rows to force in the grid
+        ncols (Optional[int]): Number of columns to try to force in the grid
+        cell_width (int): Width of the background rectangle
+        cell_height (int): Height of the background rectangle
+        dpi (int): Resolution of the legend
+        fontsize (int): Text size of each label
+        wrapsize (int): Maximum characters per line before wrapping takes effect
+        offset (int): Offset of label from edge of cell
+        alpha (int): Overall alpha of the colormap
+        sort (bool): Sort cell order alphabetically
+    """
+    N = len(colormap)
+    if nrows is None or nrows * ncols < N:
+        nrows = N // ncols + int(N % ncols > 0)
+
+    width = cell_width * ncols
+    height = cell_height * nrows
+    figsize = (width / dpi, height / dpi)
+
+    fig = plt.figure(dpi=dpi, figsize=figsize)
+    ax = fig.subplots()
+    ax.set_axis_off()
+
+    fig.subplots_adjust(0.0, 0.0, 1.0, 1.0)
+    ax.set_xlim(0, cell_width * ncols)
+    ax.set_ylim(cell_height * (nrows - 0.5), -cell_height / 2.0)
+
+    labels = sorted([x for x in colormap]) if sort else [x for x in colormap]
+    for i, label in enumerate(labels):
+        # figure out index in grid
+        col = i % ncols
+        row = i // ncols
+        # compute cell coordinates
+        start_x = cell_width * col
+        y = row * cell_height
+
+        # get background and appropriate foreground colors
+        color = colormap[label][:3]
+        prod = (1 / alpha) * functools.reduce(lambda x, y: x * y, color, 1)
+        textcolor = (0.3, 0.3, 0.3) if prod > 0.1 else (1.0, 1.0, 1.0)
+
+        text = _wrap(label, width=wrapsize)
+        ax.text(
+            start_x + offset,
+            y,
+            text,
+            fontsize=fontsize,
+            horizontalalignment="left",
+            verticalalignment="center",
+            color=textcolor,
+        )
+
+        ax.add_patch(
+            Rectangle(
+                xy=(start_x, y - cell_height / 2),
+                width=cell_width,
+                height=cell_height,
+                facecolor=color,
+                alpha=alpha,
+            )
+        )
+
+    return fig
