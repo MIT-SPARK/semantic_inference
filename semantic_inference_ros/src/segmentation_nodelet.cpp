4407d8e2e9c036aafd95a56b9021c13a70c40f4a
Nathan Hughes <nathan.h.hughes@gmail.com>
2024-11-28 14:14:30 -0500

    upgrade segmentation nodelets

--- semantic_inference_ros/src/segmentation_nodelet.cpp
+++ semantic_inference_ros/src/segmentation_nodelet.cpp
@@ -30,16 +30,13 @@
  * * -------------------------------------------------------------------------- */
 
 #include <config_utilities/config_utilities.h>
-#include <config_utilities/parsing/ros.h>
-#include <cv_bridge/cv_bridge.h>
-#include <image_transport/image_transport.h>
-#include <nodelet/nodelet.h>
-#include <pluginlib/class_list_macros.h>
 #include <semantic_inference/image_rotator.h>
 #include <semantic_inference/model_config.h>
 #include <semantic_inference/segmenter.h>
 
 #include <atomic>
+#include <cv_bridge/cv_bridge.hpp>
+#include <image_transport/image_transport.hpp>
 #include <mutex>
 #include <opencv2/core.hpp>
 #include <optional>
@@ -51,9 +48,11 @@
 
 namespace semantic_inference {
 
-class SegmentationNodelet : public nodelet::Nodelet {
+using sensor_msgs::msg::Image;
+
+class SegmentationNode : public rclcpp::Node {
  public:
-  using ImageWorker = Worker<sensor_msgs::ImageConstPtr>;
+  using ImageWorker = Worker<Image::ConstSharedPtr>;
 
   struct Config {
     Segmenter::Config segmenter;
@@ -62,12 +61,12 @@ class SegmentationNodelet : public nodelet::Nodelet {
     ImageRotator::Config image_rotator;
   };
 
-  virtual void onInit() override;
+  explicit SegmentationNode(const rclcpp::NodeOptions& options);
 
-  virtual ~SegmentationNodelet();
+  virtual ~SegmentationNode();
 
  private:
-  void runSegmentation(const sensor_msgs::ImageConstPtr& msg);
+  void runSegmentation(const Image::ConstSharedPtr& msg);
 
   Config config_;
   std::unique_ptr<Segmenter> segmenter_;
@@ -79,20 +78,20 @@ class SegmentationNodelet : public nodelet::Nodelet {
   image_transport::Subscriber sub_;
 };
 
-void declare_config(SegmentationNodelet::Config& config) {
+void declare_config(SegmentationNode::Config& config) {
   using namespace config;
-  name("SegmentationNodelet::Config");
+  name("SegmentationNode::Config");
   field(config.segmenter, "segmenter");
   field(config.output, "output");
   field(config.worker, "worker");
   field(config.image_rotator, "image_rotator");
 }
 
-void SegmentationNodelet::onInit() {
-  ros::NodeHandle nh = getPrivateNodeHandle();
-  logging::Logger::addSink("ros", std::make_shared<RosLogSink>());
+SegmentationNode::SegmentationNode(const rclcpp::NodeOptions& options)
+    : Node("segmentation_node", options) {
+  logging::Logger::addSink("ros", std::make_shared<RosLogSink>(get_logger()));
 
-  config_ = config::fromRos<SegmentationNodelet::Config>(nh);
+  // config_ = config::fromRos<SegmentationNode::Config>(nh);
   SLOG(INFO) << "\n" << config::toString(config_);
   config::checkValid(config_);
 
@@ -105,7 +104,7 @@ void SegmentationNodelet::onInit() {
 
   image_rotator_ = ImageRotator(config_.image_rotator);
 
-  transport_ = std::make_unique<image_transport::ImageTransport>(nh);
+  transport_ = std::make_unique<image_transport::ImageTransport>(shared_from_this());
   output_pub_ = std::make_unique<OutputPublisher>(config_.output, *transport_);
   worker_ = std::make_unique<ImageWorker>(
       config_.worker,
@@ -116,13 +115,13 @@ void SegmentationNodelet::onInit() {
       "color/image_raw", 1, &ImageWorker::addMessage, worker_.get());
 }
 
-SegmentationNodelet::~SegmentationNodelet() {
+SegmentationNode::~SegmentationNode() {
   if (worker_) {
     worker_->stop();
   }
 }
 
-void SegmentationNodelet::runSegmentation(const sensor_msgs::ImageConstPtr& msg) {
+void SegmentationNode::runSegmentation(const Image::ConstSharedPtr& msg) {
   cv_bridge::CvImageConstPtr img_ptr;
   try {
     img_ptr = cv_bridge::toCvShare(msg, "rgb8");
@@ -149,4 +148,5 @@ void SegmentationNodelet::runSegmentation(const sensor_msgs::ImageConstPtr& msg)
 
 }  // namespace semantic_inference
 
-PLUGINLIB_EXPORT_CLASS(semantic_inference::SegmentationNodelet, nodelet::Nodelet)
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(semantic_inference::SegmentationNode)
