8d3a5031e9791ebdbc9c4ddda5fc6a61f97c8924
Nathan Hughes <nathan.h.hughes@gmail.com>
2024-11-29 14:43:37 -0500

    rename executables and start updating ros python code

--- semantic_inference_ros/semantic_inference_ros/ros_logging.py
+++ semantic_inference_ros/semantic_inference_ros/ros_logging.py
@@ -31,28 +31,30 @@
 
 from semantic_inference import Logger
 import logging
-import rospy
 
 
 # adapted from https://gist.github.com/ablakey/4f57dca4ea75ed29c49ff00edf622b38
 class RosForwarder(logging.Handler):
     """Class to forward logging to ros handler."""
 
-    level_map = {
-        logging.DEBUG: rospy.logdebug,
-        logging.INFO: rospy.loginfo,
-        logging.WARNING: rospy.logwarn,
-        logging.ERROR: rospy.logerr,
-        logging.CRITICAL: rospy.logfatal,
-    }
+    def __init__(self, node, **kwargs):
+        """Construct a logging Handler that forwards log messages to ROS."""
+        super(RosForwarder, self).__init__(**kwargs)
+        self._level_map = {
+            logging.DEBUG: node.get_logger().debug,
+            logging.INFO: node.get_logger().info,
+            logging.WARNING: node.get_logger().warning,
+            logging.ERROR: node.get_logger().error,
+            logging.CRITICAL: node.get_logger().fatal,
+        }
 
     def emit(self, record):
         """Send message to ROS."""
         level = record.levelno if record.levelno in self.level_map else logging.CRITICAL
-        self.level_map[level](f"{record.name}: {record.msg}")
+        self._level_map[level](f"{record.name}: {record.msg}")
 
 
-def setup_ros_log_forwarding(level=logging.INFO):
+def setup_ros_log_forwarding(node, level=logging.INFO):
     """Forward logging to ROS."""
-    Logger.addHandler(RosForwarder())
+    Logger.addHandler(RosForwarder(node))
     Logger.setLevel(logging.INFO)
